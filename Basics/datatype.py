c6620649b5f9585e51c09f6612eaa6574d61aa36
OnepowerOP <75950161+OnepowerOP@users.noreply.github.com>
2022-03-07 21:45:24 +0530

    Python

--- Basics/datatype.py
+++ Basics/datatype.py
@@ -1,17 +1,17 @@
-# x=4
-# y="Rishu"
-# print (type(x))
-# print (type(y))
+x=4
+y="Rishu"
+print (type(x))
+print (type(y))
 
 # x,y,z="Rishu","Priyanshu","Shremshth"
 # print(x)
 # print(y)
 # print(z)
 
-x=y=z="Hello"
-print(x)
-print(y)
-print(z)
+# x=y=z="Hello"
+# print(x)
+# print(y)
+# print(z)
 
 # x='Cheers'
 # print ("Python makes people " +x)
